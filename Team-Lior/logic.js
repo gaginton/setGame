767b4b4ca5c828a15214d577c7967e06f2f6484c
Jddanan <jddanan@gmail.com>
2018-12-06 10:02:23 +0200

    adding basic structure in React

--- Team-Lior/logic.js
+++ Team-Lior/logic.js
@@ -1,4 +1,59 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Board />
+        );
+    }
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        );
+    }
+}
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Shape />
+        );
+    }
+}
+class Shape extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className={`Color Shape Number Fill`}></div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <div>Hello World</div>,
+    <App/>,
     document.getElementById("root")
 );

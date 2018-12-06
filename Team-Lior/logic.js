f4c9bc01bfe7a8390f79a96da2755fe683bdda8d
Guy Ginton <guyginton@gmail.com>
2018-12-06 11:44:57 +0200

    fixed merge conflicts

--- Team-Lior/logic.js
+++ Team-Lior/logic.js
@@ -12,31 +12,28 @@ class App extends React.Component {
 class Board extends React.Component {
     constructor(props) {
         super(props);
+        this.getColor=this.getColor.bind(this)
+
+    }
+    getColor(e){
+        e.target.className
+        console.log(e.target.className)
     }
     render() {
         return (
-            <div>
-                <Card color="red" symbol="oval" number="1" shade="solid" />
-                <Card color="red" symbol="oval" number="3" shade="solid" />
-<<<<<<< HEAD
-                <Card color="green" symbol="diamond" number="1" shade="solid" />
-                <Card color="purple" symbol="squiggle" number="1" shade="open" />
-                <Card color="red" symbol="oval" number="3" shade="open" />
-                <Card color="green" symbol="squiggle" number="2" shade="open" />
-                <Card color="green" symbol="oval" number="3" shade="stripe" />
-                <Card color="purple" symbol="diamond" number="3" shade="open" />
-=======
-                <Card color="red" symbol="diamond" number="1" shade="solid" />
-                <Card color="red" symbol="square" number="1" shade="open" />
-                <Card color="green" symbol="oval" number="3" shade="open" />
-                <Card color="green" symbol="square" number="2" shade="open" />
-                <Card color="green" symbol="oval" number="3" shade="stripe" />
-                <Card color="green" symbol="diamond" number="3" shade="open" />
->>>>>>> ab791e781a5184c51dc4212942cc7f5b8dbad6ed
-                <Card color="purple" symbol="oval" number="2" shade="open" />
-                <Card color="purple" symbol="oval" number="2" shade="stripe" />
-                <Card color="purple" symbol="square" number="3" shade="stripe" />
-                <Card color="purple" symbol="oval" number="3" shade="solid" />
+            <div className="board">
+                <Card handleClick={this.getColor} color="red" symbol="oval" number="1" shade="solid" />
+                <Card handleClick={this.getColor} color="red" symbol="oval" number="3" shade="solid" />
+                <Card handleClick={this.getColor} color="red" symbol="diamond" number="1" shade="solid" />
+                <Card handleClick={this.getColor} color="red" symbol="square" number="1" shade="open" />
+                <Card handleClick={this.getColor} color="green" symbol="oval" number="3" shade="open" />
+                <Card handleClick={this.getColor} color="green" symbol="square" number="2" shade="open" />
+                <Card handleClick={this.getColor} color="green" symbol="oval" number="3" shade="stripe" />
+                <Card handleClick={this.getColor} color="green" symbol="diamond" number="3" shade="open" />
+                <Card handleClick={this.getColor} color="purple" symbol="oval" number="2" shade="open" />
+                <Card handleClick={this.getColor} color="purple" symbol="oval" number="2" shade="stripe" />
+                <Card handleClick={this.getColor} color="purple" symbol="square" number="3" shade="stripe" />
+                <Card handleClick={this.getColor} color="purple " symbol="oval" number="3" shade="solid" />
             </div>
         );
     }
@@ -45,6 +42,7 @@ class Board extends React.Component {
 class Card extends React.Component {
     constructor(props) {
         super(props);
+        this.select=this.select.bind(this)
         this.state = {
             color: "",
             symbol: "",
@@ -52,20 +50,14 @@ class Card extends React.Component {
             shade: "",
         }
     }
-
-
- 
-
-
+    select(e){
+        this.props.handleClick(e)
+    }
     render() {
         return (
-<<<<<<< HEAD
-            <Shape color={this.props.color} symbol={this.props.color} number={this.props.number} shade={this.props.shade} />
-=======
-            <div>
-                <Shape color={this.props.color} symbol={this.props.symbol} number={this.props.number} shade={this.props.shade} />
+            <div onClick={this.select}>
+                <Shape  color={this.props.color} symbol={this.props.symbol} number={this.props.number} shade={this.props.shade} />
             </div>
->>>>>>> ab791e781a5184c51dc4212942cc7f5b8dbad6ed
         );
     }
 }
@@ -76,11 +68,7 @@ class Shape extends React.Component {
     }
     render() {
         return (
-<<<<<<< HEAD
-            <div className={`card ${this.props.color} ${this.props.symbol} ${this.props.number} ${this.props.shade}`}></div>
-=======
-            <div style={{backgroundColor:this.props.color}} className={`card ${this.props.color} ${this.props.symbol} ${this.props.number} ${this.props.shade}`}></div>
->>>>>>> ab791e781a5184c51dc4212942cc7f5b8dbad6ed
+            <div  style={{backgroundColor:this.props.color}} className={`card ${this.props.color} ${this.props.symbol} ${this.props.number} ${this.props.shade}`}></div>
         );
     }
 }

0c5a6fffb464027e122be588f9b2991101e49117
Jddanan <jddanan@gmail.com>
2018-12-06 10:30:18 +0200

    adding hardcoded board

--- Team-Lior/logic.js
+++ Team-Lior/logic.js
@@ -16,18 +16,18 @@ class Board extends React.Component {
     render() {
         return (
             <div>
-                <Card color="red" shape="" />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card color="red" symbol="oval" number="1" shade="solid"/>
+                <Card color="red" symbol="oval" number="3" shade="solid" />
+                <Card color="green" symbol="diamond" number="1" shade="solid" />
+                <Card color="purple" symbol="squiggle" number="1" shade="open" />
+                <Card color="red" symbol="oval" number="3" shade="open" />
+                <Card color="green" symbol="squiggle" number="2" shade="open"  />
+                <Card color="green" symbol="oval" number="3" shade="stripe"/>
+                <Card color="purple" symbol="diamond" number="3" shade="open" />
+                <Card color="purple" symbol="oval" number="2" shade="open" />
+                <Card color="red" symbol="oval" number="2" shade="stripe" />
+                <Card color="red" symbol="squiggle" number="3" shade="stripe" />
+                <Card color="green" symbol="oval" number="3" shade="solid" />
             </div>
         );
     }
